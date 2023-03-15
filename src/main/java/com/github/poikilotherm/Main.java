0c2c744ae97e159ff798e616757a3209433e2773
Oliver Bertuch <o.bertuch@fz-juelich.de>
2023-03-15 15:37:10 +0100

    Add test javadoc comment to see trigger in action

--- src/main/java/com/github/poikilotherm/Main.java
+++ src/main/java/com/github/poikilotherm/Main.java
@@ -1,6 +1,11 @@
 package com.github.poikilotherm;
 
 public class Main {
+    
+    /**
+     * This is a test method.
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
